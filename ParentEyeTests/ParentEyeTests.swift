3713db58b75469eb4d37eb0376cc5ef82496127c
Yuanyuan Shi <yuanyuanshi111@gmail.com>
2024-10-14 10:31:30 -0700

    Initial Commit

---
+++ ParentEyeTests/ParentEyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParentEyeTests.swift
+//  ParentEyeTests
+//
+//  Created by Yuanyuan on 10/14/24.
+//
+
+import Testing
+@testable import ParentEye
+
+struct ParentEyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
