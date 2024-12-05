eccbc7d8802be72a8acc8839faf32ec3769d298e
YuanyuanGoGo <59780682+andreashi2020@users.noreply.github.com>
2024-12-05 15:30:43 -0800

    Update AppDelegate.swift about api key

--- ParentEye/AppDelegate.swift
+++ ParentEye/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyAXWipZnTV-fRm5eZ5HdFOyCl1sT841jV4")
+        GMSServices.provideAPIKey("Replace with your API key")
         return true
     }
 
